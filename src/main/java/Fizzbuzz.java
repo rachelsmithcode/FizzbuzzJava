1a93ecf722661345c578239619d5c4a7dfd81abc
Leo Allen <leo.f.allen@gmail.com>
2016-04-15 18:42:19 +0100

    Testing PRs

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -4,8 +4,8 @@
 public class Fizzbuzz {
     public static String getResults(int number) {
         if (number % 15 == 0) return "fizzbuzz";
-        if (number % 3 == 0 ) return "fizz";
-        if (number % 5 == 0 ) return "buzz";
+        if (number % 3 == 0) return "fizz";
+        if (number % 5 == 0) return "buzz";
         return Integer.toString(number);
     }
 }
